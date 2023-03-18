c022c5dc7a5f4282239bf7360458ce87c74d1dbf
Justin Marwad <justinmarwad@gmail.com>
2023-03-18 15:41:19 -0400

    Fixed Zoom downloads, other major changes

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 Zoom2YouTube brings together a few different projects and provides you with an easy project that lets you download recorded Zoom meetings, edit a logo into them, and upload them to YouTube directly. 
 
+### Latest Update Notes ### 
+
+I'm happy to announce that I've updated this project to fix automatic Zoom downloads! You will need to create a server-to-server OAuth app in Zoom and then add the account ID, client ID, and client secret to the .env file. Additionally, I had to make sure that in the code I created an account authentication vs client authentication. You don't have to worry about that though. 
+
+I'll update the documentation in my next update to give instructions on how to create a server-to-server OAuth app in Zoom. You should be able to Google it (or Bing Chat it!) for now and be fine though. The next update on my pipeline is to enable automatic YouTube uploads. I made some progress here and added in a new library, but I will need to figure out automatic OAuth authentication for this (right now that would have to be done manually, which would prevent this script from being run automatically).  
 
 ### Documentation ###
 
@@ -18,7 +23,7 @@ Makes use of the following two projects:
 
 MIT License
 
-Copyright (c) 2020 tribloom
+Copyright (c) 2023 Justin Marwad
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- Zoom2YouTube.py
+++ Zoom2YouTube.py
@@ -1,14 +1,47 @@
 # Justin Marwad 2022 
+import os, subprocess, logging, colorama, pathlib, sys, base64
 from multiprocessing import parent_process
-import os, subprocess, logging, colorama, pathlib 
 import dateutil.parser as dparser
+import requests, json
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+
+from youtube_upload.client import YoutubeUploader
 
 class Zoom2YouTube:
-    def __init__(self, email, from_date, to_date): 
-        self.email = email 
+    def __init__(self, email=None, from_date=None, to_date=None, gpu_encode=False):
+        # Load environment variables
+        load_dotenv()
 
+        if not from_date: 
+            from_date = os.environ.get("FROM_DATE")
+        if not to_date:
+            to_date = os.environ.get("TO_DATE")
+        if not email: 
+            email = os.environ.get("EMAIL")
+        
+        ZOOM_ACCOUNT_ID = os.environ.get("ZOOM_ACCOUNT_ID")
+        ZOOM_CLIENT_ID = os.environ.get("ZOOM_CLIENT_ID")
+        ZOOM_CLIENT_SECRET = os.environ.get("ZOOM_CLIENT_SECRET")
+        self.ZOOM_MEETING_ID = os.environ.get("ZOOM_MEETING_ID")
+        self.YOUTUBE_CLIENT_SECRETS="secrets/client_secrets.json" 
+
+        # if from_date and to_date not set, set them to 7 days ago and today
+        if not from_date: 
+            one_week_ago = datetime.now() - timedelta(days=7)
+            from_date = one_week_ago.strftime('%Y-%m-%d')
+        if not to_date: 
+            to_date = datetime.now().strftime('%Y-%m-%d')
+
+        # Set variables
+        self.email = email 
         self.from_date = from_date 
         self.to_date = to_date
+
+        self.gpu_encode = gpu_encode
+
+        self.ZOOM_ACCESS_TOKEN = self.create_zoom_access_token(ZOOM_ACCOUNT_ID, ZOOM_CLIENT_ID, ZOOM_CLIENT_SECRET)
+        self.save_downloads_location = "recordings"
 
 
     ## Utility methods ##
@@ -36,52 +69,116 @@ class Zoom2YouTube:
 
         return path
 
+    def create_zoom_access_token(self, ZOOM_ACCOUNT_ID, ZOOM_CLIENT_ID, ZOOM_CLIENT_SECRET):
+        """ Create a Zoom OAuth access token. """
+
+        base64_encoded = base64.b64encode(f"{ZOOM_CLIENT_ID}:{ZOOM_CLIENT_SECRET}".encode()).decode()
+
+        response = requests.request("POST", 
+            "https://zoom.us/oauth/token", 
+            headers={
+                'Host': 'zoom.us',
+                'Authorization': f'Basic {base64_encoded}',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }, 
+            data=f'grant_type=account_credentials&account_id={ZOOM_ACCOUNT_ID}'
+        )
+        
+        response.raise_for_status()
+        return response.json()['access_token']
+
     ## Main code ##
     def download_video(self, secrets="secrets/zoom_secrets.json"): 
         """ Download all zoom recordings from a given date range. """
-        self.run(f"python3 zoom_meeting_download.py -e {email} -f {self.from_date} -t {se.fto_date} -s {secrets}")
+        # self.run(f"python3 zoom_meeting_download.py -e {email} -f {self.from_date} -t {self.to_date} -s {secrets}")
+
+
+        # Download each recording 
+        response = requests.get( 
+            url=f'https://api.zoom.us/v2/users/{self.email}/recordings?from={self.from_date}&to={self.to_date}', 
+             headers={'Authorization': f'Bearer {self.ZOOM_ACCESS_TOKEN}'}, 
+         
+        )
+        
+        response.raise_for_status()
+        meetings = response.json()["meetings"]
+
+        # Download the recordings
+        for meeting in meetings: 
+            for file in meeting["recording_files"]:
+                if file["file_type"] == "MP4":
+                    download_url = file["download_url"]
+                    save_place = f"{self.save_downloads_location}/{file['recording_start']}.mp4"
+
+                    self.result(f"[+] Found recording on {file['recording_start']} to {file['recording_end']} at the location '{download_url}' and saving to '{save_place}'")
+
+                    response = requests.get(download_url)
+                    with open(save_place, "wb") as f:
+                        f.write(response.content)
 
     def edit_video(self, intro_video="logo.mp4", outro_video="logo.mp4", final_video=None):
         """ Edit video with intro and outro. """
 
         for root, dirs, files in os.walk("recordings"):
             for video in files:
-                if video.endswith(".mp4"):
-                    
+                if video.endswith(".mp4"):                   
                     video_path = os.path.join(root, video)
-                    parent_dir = os.path.basename(os.path.dirname(video_path))
-                    date = dparser.parse(parent_dir.split(" ")[0], fuzzy=True).strftime("%Y-%m-%d")
+                    date = datetime.strptime(video, "%Y-%m-%dT%H:%M:%SZ.mp4")
+                    date = date.strftime("%Y-%m-%dT%H:%M:%SZ")   
 
-                    if date > self.from_date and date < self.to_date:
-                        self.result(f"Editing {video}")
+                    # if date >= self.from_date and date <= self.to_date:
+
+                    self.result(f"Editing {video}")
+                    
+                    # if final_video is None: 
+                    final_video = f"final/final_{date}.mp4"
+
+                    gpu_encode_flags = ""
+                    if self.gpu_encode: 
+                        # gpu_encode_flags = "-hwaccel cuda -hwaccel_output_format cuda -c:v h264_nvenc"    
+                        gpu_encode_flags = "-hwaccel cuda"    
                         
-                        if final_video is None: 
-                            final_video = self.uniquify(f"final/final_{date}.mp4")
 
-                        self.run(f"""ffmpeg -i {intro_video} -i "{video_path}" -i {outro_video} -filter_complex '[0:v]setsar=1,fps=30000/1001,format=yuv420p[intro];[1:v]scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:-1:-1,setsar=1,fps=30000/1001,format=yuv420p[video];[2:v]setsar=1,fps=30000/1001,format=yuv420p[outro];[0:a]aformat=sample_rates=48000:channel_layouts=stereo[introa];[1:a]aformat=sample_rates=48000:channel_layouts=stereo[videoa];[2:a]aformat=sample_rates=48000:channel_layouts=stereo[outroa];[intro][introa][video][videoa][outro][outroa]concat=n=3:v=1:a=1[vid][a]' -map '[vid]' -map '[a]' -movflags +faststart {final_video} < /dev/null""")
+                    self.run(f"""ffmpeg {gpu_encode_flags} -i {intro_video} -i "{video_path}" -i {outro_video} -filter_complex '[0:v]setsar=1,fps=30000/1001,format=yuv420p[intro];[1:v]scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:-1:-1,setsar=1,fps=30000/1001,format=yuv420p[video];[2:v]setsar=1,fps=30000/1001,format=yuv420p[outro];[0:a]aformat=sample_rates=48000:channel_layouts=stereo[introa];[1:a]aformat=sample_rates=48000:channel_layouts=stereo[videoa];[2:a]aformat=sample_rates=48000:channel_layouts=stereo[outroa];[intro][introa][video][videoa][outro][outroa]concat=n=3:v=1:a=1[vid][a]' -map '[vid]' -map '[a]' -movflags +faststart {final_video} < /dev/null""")
 
-                        self.result(f"Finished editing {video}")
-                        self.run(f"rm {video_path}")
-                        self.result(f"Removed {video}")
-                    else: 
-                        self.result("[-] ERROR: No videos with dates that match. Skipping editing.", colorama.Fore.RED)
+                    self.result(f"Finished editing {video}")
+                    self.run(f"rm {video_path}")
+                    self.result(f"Removed {video}")
 
-    def upload_video(self, video, privacy="unlisted", meta_token="secrets/meta.json", client_secrets="secrets/client_secrets.json", request_token="secrets/request_token.json"):
+                    # else: 
+                    #     self.result("[-] ERROR: No videos with dates that match. Skipping editing.", colorama.Fore.RED)
+
+    def upload_video(self, video, privacy="private"):
         """ Upload video to YouTube. """
-        self.run(f"./youtubeuploader -headlessAuth -filename '{video}' -title '{video}' -privacy '{privacy}' -metaJSON '{meta_token}' -secrets '{client_secrets}' -cache '{request_token}'")
+
+        uploader = YoutubeUploader(secrets_file_path=self.YOUTUBE_CLIENT_SECRETS)
+        uploader.authenticate()
+
+        # Video options
+        options = {
+            "title" : f"{video} | Title ", # The video title
+            "description" : "Description", # The video description
+            # "tags" : ["tag1", "tag2", "tag3"],
+            # "categoryId" : "22",
+            "privacyStatus" : "private", # "public", "private", or "unlisted"
+            "kids" : False, # Specifies if the Video if for kids or not. Defaults to False.
+            # "thumbnailLink" : "https://awebsite/animage.jpg" # Optional. Specifies video thumbnail.
+        }
+
+        # upload video
+        uploader.upload(video, options) 
+
 
 
 if __name__ == "__main__":
-    if os.environ["EMAIL"]:
-        email = os.environ["EMAIL"] 
 
     print("[INFO] Zoom2YouTube. Download, edit, and upload Zoom recordings to YouTube.") 
-    z2y = Zoom2YouTube(email, "2022-09-18", "2022-09-22")
+    z2y = Zoom2YouTube(gpu_encode=False)
     
     # print("[INFO] Downloading recordings from Zoom.")
-    # z2y.download_video()
+    z2y.download_video()
     
-    print("[INFO] Editing recordings.")
+    # print("[INFO] Editing recordings.")
     z2y.edit_video()
 
     # print("[INFO] Uploading recordings.")
